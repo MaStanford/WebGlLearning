e0908adc9e26f3e21b2ff0a126fdec1c76ceb753
Mark Stanford <m.stanford@samsung.com>
2015-04-20 16:16:45 -0700

    Spinning box works

--- app.js
+++ app.js
@@ -1,77 +1,154 @@
-//Init is being called from the onload in the html
-// window.addEventListener("load", function() {
-	// init();
-// });
 var g = {};
 
-function initWebGL(canvas) {
-	gl = null;
-
-	try {
-		// Try to grab the standard context. If it fails, fallback to experimental.
-		gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-	} catch(e) {
-		alert("Fail to get context for webgl.");
-	}
-
-	// If we don't have a GL context, give up now
+function init() {
+	// Initialize
+	var gl = initWebGL(
+	// The id of the Canvas Element
+	"example");
 	if (!gl) {
-		alert("Unable to initialize WebGL. Your browser may not support it.");
-		gl = null;
+		return;
 	}
+	var program = simpleSetup(gl,
+	// The ids of the vertex and fragment shaders
+	"vshader", "fshader",
+	// The vertex attribute names used by the shaders.
+	// The order they appear here corresponds to their index
+	// used later.
+	["vNormal", "vColor", "vPosition"],
+	// The clear color and depth values
+	[0, 0, 0.5, 1], 10000);
+
+	// Set some uniform variables for the shaders
+	gl.uniform3f(gl.getUniformLocation(program, "lightDir"), 0, 0, 1);
+	gl.uniform1i(gl.getUniformLocation(program, "sampler2d"), 0);
+
+	// Create a box. On return 'gl' contains a 'box' property with
+	// the BufferObjects containing the arrays for vertices,
+	// normals, texture coords, and indices.
+	g.box = makeBox(gl);
+
+	// Load an image to use. Returns a WebGLTexture object
+	spiritTexture = loadImageTexture(gl, "resources/image.jpeg");
+
+	// Create some matrices to use later and save their locations in the shaders
+	g.mvMatrix = new J3DIMatrix4();
+	g.u_normalMatrixLoc = gl.getUniformLocation(program, "u_normalMatrix");
+	g.normalMatrix = new J3DIMatrix4();
+	g.u_modelViewProjMatrixLoc = gl.getUniformLocation(program, "u_modelViewProjMatrix");
+	g.mvpMatrix = new J3DIMatrix4();
+
+	// Enable all of the vertex attribute arrays.
+	gl.enableVertexAttribArray(0);
+	gl.enableVertexAttribArray(1);
+	gl.enableVertexAttribArray(2);
+
+	// Set up all the vertex attributes for vertices, normals and texCoords
+	gl.bindBuffer(gl.ARRAY_BUFFER, g.box.vertexObject);
+	gl.vertexAttribPointer(2, 3, gl.FLOAT, false, 0, 0);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, g.box.normalObject);
+	gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, g.box.texCoordObject);
+	gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 0, 0);
+
+	// Bind the index array
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, g.box.indexObject);
 
 	return gl;
 }
 
-function init() {
-	canvas = document.getElementById("glcanvas");
+var requestId;
+
+function reshape(gl) {
+	// change the size of the canvas's backing store to match the size it is displayed.
+	var canvas = document.getElementById('example');
+	if (canvas.clientWidth == canvas.width && canvas.clientHeight == canvas.height)
+		return;
+
+	canvas.width = canvas.clientWidth;
+	canvas.height = canvas.clientHeight;
+
+	// Set the viewport and projection matrix for the scene
+	gl.viewport(0, 0, canvas.clientWidth, canvas.clientHeight);
+	g.perspectiveMatrix = new J3DIMatrix4();
+	g.perspectiveMatrix.perspective(30, canvas.clientWidth / canvas.clientHeight, 1, 10000);
+	g.perspectiveMatrix.lookat(0, 0, 7, 0, 0, 0, 0, 1, 0);
+}
+
+function drawPicture(gl) {
+	// Make sure the canvas is sized correctly.
+	reshape(gl);
+
+	// Clear the canvas
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	// Make a model/view matrix.
+	g.mvMatrix.makeIdentity();
+	g.mvMatrix.rotate(20, 1, 0, 0);
+	g.mvMatrix.rotate(currentAngle, 0, 1, 0);
+
+	// Construct the normal matrix from the model-view matrix and pass it in
+	g.normalMatrix.load(g.mvMatrix);
+	g.normalMatrix.invert();
+	g.normalMatrix.transpose();
+	g.normalMatrix.setUniform(gl, g.u_normalMatrixLoc, false);
+
+	// Construct the model-view * projection matrix and pass it in
+	g.mvpMatrix.load(g.perspectiveMatrix);
+	g.mvpMatrix.multiply(g.mvMatrix);
+	g.mvpMatrix.setUniform(gl, g.u_modelViewProjMatrixLoc, false);
+
+	// Bind the texture to use
+	gl.bindTexture(gl.TEXTURE_2D, spiritTexture);
+
+	// Draw the cube
+	gl.drawElements(gl.TRIANGLES, g.box.numIndices, gl.UNSIGNED_BYTE, 0);
+
+	// Show the framerate
+	framerate.snapshot();
+
+	currentAngle += incAngle;
+	if (currentAngle > 360)
+		currentAngle -= 360;
+}
+
+function start() {
+	var c = document.getElementById("example");
 	
-    // Initialize
-    var gl = initWebGL(canvas);
-    
-    if (!gl) {
-        return;
-    }
- 
-    g.program = simpleSetup(
-            gl, "vshader", "fshader", 
-            [ "vNormal", "vColor", "vPosition"], [ 0, 0, 0, 1 ], 10000);
- 
-    // Set some uniform variables for the shaders
-    gl.uniform3f(gl.getUniformLocation(g.program, "lightDir"), 0, 0, 1);
-    gl.uniform1i(gl.getUniformLocation(g.program, "sampler2d"), 0);
- 
-    // Create a box. with the BufferObjects containing the arrays 
-    // for vertices, normals, texture coords, and indices.
-    g.box = makeBox(gl);
- 
-    // Load an image to use. Returns a WebGLTexture object
-    spiritTexture = loadImageTexture(gl, "resources/image.jpeg");
- 
-    // Create some matrices to use later and save their locations in the shaders
-    g.mvMatrix = new J3DIMatrix4();
-    g.u_normalMatrixLoc = gl.getUniformLocation(g.program, "u_normalMatrix");
-    g.normalMatrix = new J3DIMatrix4();
-    g.u_modelViewProjMatrixLoc = gl.getUniformLocation(g.program, "u_modelViewProjMatrix");
-    g.mvpMatrix = new J3DIMatrix4();
- 
-    // Enable all of the vertex attribute arrays.
-    gl.enableVertexAttribArray(0);
-    gl.enableVertexAttribArray(1);
-    gl.enableVertexAttribArray(2);
- 
-    // Set up all the vertex attributes for vertices, normals and texCoords
-    gl.bindBuffer(gl.ARRAY_BUFFER, g.box.vertexObject);
-    gl.vertexAttribPointer(2, 3, gl.FLOAT, false, 0, 0);
- 
-    gl.bindBuffer(gl.ARRAY_BUFFER, g.box.normalObject);
-    gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
- 
-    gl.bindBuffer(gl.ARRAY_BUFFER, g.box.texCoordObject);
-    gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 0, 0);
- 
-    // Bind the index array
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, g.box.indexObject);
- 
-    return gl;
+	c.onclick = function(){
+		alert('Clickety Clack');
+	};
+
+	c.addEventListener('webglcontextlost', handleContextLost, false);
+	c.addEventListener('webglcontextrestored', handleContextRestored, false);
+
+	var gl = init();
+	if (!gl) {
+		return;
+	}
+
+	currentAngle = 0;
+	incAngle = 0.5;
+	framerate = new Framerate("framerate");
+	var f = function() {
+		drawPicture(gl);
+		requestId = window.requestAnimFrame(f, c);
+	};
+	f();
+
+	function handleContextLost(e) {
+		e.preventDefault();
+		clearLoadingImages();
+		if (requestId !== undefined) {
+			window.cancelAnimFrame(requestId);
+			requestId = undefined;
+		}
+	}
+
+	function handleContextRestored() {
+		init();
+		f();
+	}
+
 }
